d34e402a94029e0827f50d33f6a30e96417ab273
rajsingh8220 <raj.singh822014@gmail.com>
2016-05-11 22:26:32 -0500

    first commit

---
+++ src/main/java/com/dams/controller/admin/AdminController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dams.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author raj
+ */
+@Controller
+public class AdminController {
+     @RequestMapping("/admin")
+    public String start(){
+        return "redirect:/admin"; // How would it differ if it was forward instead?
+    }
+    
+    @RequestMapping("/admin")
+    public String welcome(Model model){
+        return "admin";
+    }
+}
---
+++ src/main/java/com/dams/controller/client/ClientController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dams.controller.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author raj
+ */
+@Controller
+public class ClientController {
+    
+    @RequestMapping("/")
+    public String start(){
+        return "redirect:/client"; // How would it differ if it was forward instead?
+    }
+    
+    @RequestMapping("/client")
+    public String welcome(Model model){
+        return "client";
+    }
+    
+}
