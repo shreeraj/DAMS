fb7674cbf4ad30c09eb97fb47435dd48a54c9dfc
rajsingh8220 <raj.singh822014@gmail.com>
2016-05-16 02:08:31 -0500

    Patient CRUD Except UI

---
+++ src/main/java/com/dams/model/PatientDAO.java
@@ -0,0 +1,56 @@
+package com.dams.model;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import com.dams.domain.Patient;
+import com.dams.repository.PatientRepository;
+import com.dams.service.PatientService;
+
+
+@Service
+@Transactional
+public class PatientDAO implements PatientService{
+
+	@Autowired
+	private PatientRepository patientRepository;
+	
+	@Override
+	public List<Patient> getAll() {
+		return (List<Patient>) patientRepository.findAll();
+	}
+
+	@Override
+	public void savePatient(Patient patient) {
+		patientRepository.save(patient);
+		
+	}
+
+	@Override
+	public Patient findById(int docId) {
+		return patientRepository.findOne(docId);
+	}
+
+	@Override
+	public void updatePatient(Patient patient) {
+		patientRepository.save(patient);
+		
+	}
+
+	@Override
+	public void deletePatient(Patient patient) {
+		patientRepository.delete(patient);
+		
+	}
+
+	@Override
+	public Patient findByFirstname(String firstname) {
+		return patientRepository.findByFirstname(firstname);
+	}
+
+}
