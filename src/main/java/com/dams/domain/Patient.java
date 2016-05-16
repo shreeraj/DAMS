c4d197a1cd1d6252b797b365eb067c2fa333aef8
rajsingh8220 <raj.singh822014@gmail.com>
2016-05-16 01:49:26 -0500

    patient domain push test for shailesh branch

--- src/main/java/com/dams/domain/Patient.java
+++ src/main/java/com/dams/domain/Patient.java
@@ -11,53 +11,37 @@ import javax.persistence.Id;
 
 /**
  *
- * @author shreerajkarki
+ * @author raj
  */
 @Entity
-public class Doctor extends Address{
+public class Patient extends Address{
     @Id
     @GeneratedValue
-    private int doctorId;
+    private int patientId;
     private String firstname;
     private String lastname;
-    private String username;
-    private String password;
+   
     private String phone;
     private String dob;
     private String email;
-    private String registration;
 
-    public Doctor() {
-    }
-    
-      
-    
-    public Doctor(String firstname, String lastname, String username, String password, String phone, String dob,
-			String email, String registration) {
-		
-		this.firstname = firstname;
-		this.lastname = lastname;
-		this.username = username;
-		this.password = password;
-		this.phone = phone;
-		this.dob = dob;
-		this.email = email;
-		this.registration = registration;
-	}
-
-
-
-
-
-
-
-
-	public int getDoctorId() {
-        return doctorId;
+    public Patient() {
     }
 
-    public void setDoctorId(int doctorId) {
-        this.doctorId = doctorId;
+    public Patient(String firstname, String lastname, String phone, String dob, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phone = phone;
+        this.dob = dob;
+        this.email = email;
+    }
+    
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
     }
 
     public String getFirstname() {
@@ -76,22 +60,6 @@ public class Doctor extends Address{
         this.lastname = lastname;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getPhone() {
         return phone;
     }
@@ -108,27 +76,13 @@ public class Doctor extends Address{
         this.dob = dob;
     }
 
-    public String getRegistration() {
-        return registration;
+    public String getEmail() {
+        return email;
     }
 
-    public void setRegistration(String registration) {
-        this.registration = registration;
+    public void setEmail(String email) {
+        this.email = email;
     }
-
-
-
-	public String getEmail() {
-		return email;
-	}
-
-
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-    
-    
     
     
 }
